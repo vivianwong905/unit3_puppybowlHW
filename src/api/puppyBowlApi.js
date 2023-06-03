a2e93699153ac49711008869178bd2c34e42d823
sagar11sachin <124698334+sagar11sachin@users.noreply.github.com>
2023-06-04 02:49:10 +0530

    Update puppyBowlApi.js

--- src/api/puppyBowlApi.js
+++ src/api/puppyBowlApi.js
@@ -9,17 +9,17 @@ export const puppyBowlApi = createApi({
   // Define a base query function that all endpoints will use as the base of their request
   baseQuery: fetchBaseQuery({
     // The base URL for all requests
-    baseUrl: "https://fsa-puppy-bowl.herokuapp.com/api/2109-UNF-HY-WEB-PT/",
+    
   }),
   // Define endpoints for our API service
   endpoints: (builder) => ({
     // Define an endpoint that fetches players
-    fetchPlayers: builder.query({
+    ({
       // The part of the URL that comes after the baseUrl for this specific endpoint
-      query: () => `players`,
+      
     }),
   }),
 });
 
 // Export hooks for each endpoint - in this case, a React hook that triggers the fetchPlayers query
-export const { useFetchPlayersQuery } = puppyBowlApi;
+
