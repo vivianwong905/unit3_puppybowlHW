c7f978bf26a95f637917bbff5f347dd67f902060
vivian wong <32468366+vivianwong905@users.noreply.github.com>
2023-10-02 18:58:05 -0400

    puppybowl hw

--- src/api/puppyBowlApi.js
+++ src/api/puppyBowlApi.js
@@ -9,17 +9,21 @@ export const puppyBowlApi = createApi({
   // Define a base query function that all endpoints will use as the base of their request
   baseQuery: fetchBaseQuery({
     // The base URL for all requests
-    
+    baseUrl: "https://fsa-puppy-bowl.herokuapp.com/api/2109-UNF-HY-WEB-PT/",
+    prepareHeaders: (headers) =>
+      headers.set("Content-Type", "application/json"),
   }),
   // Define endpoints for our API service
-  endpoints: (builder) => ({
+  endpoints: (builder) => ({ //({}) this is a single line returning an object
     // Define an endpoint that fetches players
-    ({
-      // The part of the URL that comes after the baseUrl for this specific endpoint
-      
+    fetchPlayers: builder.query({ // builder.query is how we define the endpoint - so this is call fetchPlayers
+      // The part of the URL that comes AFTER the baseUrl for this specific endpoint
+      query: () => "players", // the path - specific where the path is 
     }),
   }),
 });
 
 // Export hooks for each endpoint - in this case, a React hook that triggers the fetchPlayers query
+export default puppyBowlApi;
 
+export const { useFetchPlayersQuery } = puppyBowlApi; //the word useFetchPlayersQuery is something that we name
--- src/features/players/Players.jsx
+++ src/features/players/Players.jsx
@@ -1,53 +1,52 @@
-// Import the React library
-
+import React from "react"; // Import the React library
 
 // Import the generated hook from our RTK Query API slice
-
-
+import { useFetchPlayersQuery } from "../../api/puppyBowlApi";
 // Import the CSS styles for this component
-
-
+import "../../index.css";
 // Define a new React component
 const Players = () => {
   // Use the generated hook to fetch data from the API
   // When the component is first rendered, it will start the API fetch
   // It will re-render each time the fetch status changes (e.g., "loading", "data arrived", "error")
   const { data = {}, error, isLoading } = useFetchPlayersQuery();
+  // console.log(data.data.players); //i have error here - not sure why
+  
 
   // Show a loading message while data is being fetched
   if (isLoading) {
-    
+    return <div>Loading...</div>;
   }
 
   // Show an error message if the fetch failed
   if (error) {
-    
+    return <div>Error:{error.message}</div>
   }
 
   // Show the fetched data after it has arrived
   return (
     <div className="players">
-      
-
       {/* Map through the data array and generate a div for each player */}
-      {data.data.players.map((player) => (
+      {data.data.players.map((player) => {
+        console.log(player, "single p" );
+        return (
         // Use the player's ID as the key for this div
         <div key={player.id} className="player-card">
-          {/* Display the player's image, with the player's name as alt text */}
-          
+          <img className="player-image" src={player.imageUrl} />
+
           <div className="player-details">
-            
-            <h2>  {/* Display the player's name */} </h2> 
-            
-            <p>  {/* Display the player's breed */} </p> 
-            
-            <p> {/* Display the player's status */} </p>
+            <h2> {player.name} </h2>
+
+            <p> {player.breed} </p>
+
+            <p> {player.status} </p>
           </div>
         </div>
-      ))}
+      );
+        })}
     </div>
   );
 };
 
 // Export the component so it can be imported and used in other files
-
+export default Players;
